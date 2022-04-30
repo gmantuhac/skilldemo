916a1a519a0e355b75259bc3587c42e643ca1cfb
gmantuhac <103173107+gmantuhac@users.noreply.github.com>
2022-04-29 22:42:42 -0700

    JUnit Files

--- Demo.java
+++ Demo.java
@@ -1,3 +1,5 @@
 public class Demo {
-
+    public static int mod(int num) {
+        return num % 2;
+    }
 }
---
+++ DemoTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+public class DemoTest {
+    @Test
+    public void modTest() {
+        assertEquals(1, Demo.mod(2));
+    }
+}
